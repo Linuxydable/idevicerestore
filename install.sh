eb73b073454030df979de35222480aedf3dc7348
John (Linuxydable) BENARD <linuxydable@linuxydable.fr>
2023-06-12 13:15:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,56 +1,22 @@
 #!/bin/ash
 
-apk update && apk add --no-cache \
-git \
-build-base \
-make \
-automake \
-autoconf \
-libtool \
-pkgconf \
-openssl \
-curl \
-libusb \
-libzip \
-readline \
-openssl-dev \
-curl-dev \
-libusb-dev \
-libzip-dev \
-python3-dev \
-#python-dev \
-readline-dev \
-libxml2-dev
+apk update
+
+apk add --no-cache git build-base make automake autoconf libtool pkgconf openssl curl libusb libzip readline openssl-dev curl-dev libusb-dev libzip-dev python3-dev readline-dev libxml2-dev
 
 
-git clone https://github.com/libimobiledevice/libplist.git /usr/src/libplist && \
-cd /usr/src/libplist && \
-./autogen.sh && make && make install 
+git clone https://github.com/libimobiledevice/libplist.git /usr/src/libplist && cd /usr/src/libplist && ./autogen.sh && make && make install
 
-git clone https://github.com/libimobiledevice/libimobiledevice-glue.git /usr/src/libimobiledevice-glue && \
-cd /usr/src/libimobiledevice-glue && \
-./autogen.sh && make && make install
+git clone https://github.com/libimobiledevice/libimobiledevice-glue.git /usr/src/libimobiledevice-glue && cd /usr/src/libimobiledevice-glue && ./autogen.sh && make && make install
 
-git clone https://github.com/libimobiledevice/libusbmuxd.git /usr/src/libusbmuxd && \
-cd /usr/src/libusbmuxd && \
-./autogen.sh && make && make install
+git clone https://github.com/libimobiledevice/libusbmuxd.git /usr/src/libusbmuxd && cd /usr/src/libusbmuxd && ./autogen.sh && make && make install
 
-git clone https://github.com/libimobiledevice/libimobiledevice.git /usr/src/libimobiledevice && \
-cd /usr/src/libimobiledevice && \
-./autogen.sh && make && make install
+git clone https://github.com/libimobiledevice/libimobiledevice.git /usr/src/libimobiledevice && cd /usr/src/libimobiledevice && ./autogen.sh && make && make install
 
-git clone https://github.com/libimobiledevice/usbmuxd.git /usr/src/usbmuxd && \
-cd /usr/src/usbmuxd && git checkout 1.1.1 && \
-./autogen.sh && make && make install
+git clone https://github.com/libimobiledevice/usbmuxd.git /usr/src/usbmuxd && cd /usr/src/usbmuxd && ./autogen.sh && make && make install
 
-git clone https://github.com/libimobiledevice/libirecovery.git /usr/src/libirecovery && \
-cd /usr/src/libirecovery && \
-./autogen.sh && make && make install
+git clone https://github.com/libimobiledevice/libirecovery.git /usr/src/libirecovery && cd /usr/src/libirecovery && ./autogen.sh && make && make install
 
-git clone https://github.com/libimobiledevice/idevicerestore.git /usr/src/idevicerestore && \
-cd /usr/src/idevicerestore && \
-./autogen.sh && make && make install 
+git clone https://github.com/libimobiledevice/idevicerestore.git /usr/src/idevicerestore && cd /usr/src/idevicerestore && ./autogen.sh && make && make install 
 
-git clone https://github.com/libimobiledevice/libideviceactivation.git /usr/src/libideviceactivation && \
-cd /usr/src/libideviceactivation && \
-./autogen.sh && make && make install
+git clone https://github.com/libimobiledevice/libideviceactivation.git /usr/src/libideviceactivation && cd /usr/src/libideviceactivation && ./autogen.sh && make && make install
